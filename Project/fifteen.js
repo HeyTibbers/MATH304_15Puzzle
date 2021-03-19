52388b61608f708c652416b0dcd234194c7440a5
yzuo <yifan_zuo@sfu.ca>
2021-03-20 00:05:29 +0800

    add record On/Off button

--- Project/fifteen.js
+++ Project/fifteen.js
@@ -3,20 +3,30 @@
 	var empty_x = 3;
 	var empty_y = 3;
 	var logStack = [];
+	var logOn = false;
 	// attaches a function to the calculate button
 	window.onload = function () {
 		addBlocks();
 		document.getElementById("shufflebutton").onclick = shuffle;
 		calcPermuation();
+		document.getElementById("Move_log").onclick = function() {
+			if (logOn == false) {
+				logOn = true;
+			}
+			else {
+				logOn = false;
+				log.innerHTML = "";
+				inverseLog.innerHTML = "";
+			}
+		};
 		document.addEventListener("keypress", event => {
 			var log = document.getElementById("log");
 			var inverseLog = document.getElementById("inverseLog")
-			if (event.keyCode === 99) {
+			if (event.keyCode === 99 && logOn == true) {
 				log.innerHTML = "";
 				inverseLog.innerHTML = "";
-				logStack.splice(0,logStack.length);		// empty log stack 
 			}
-			if (event.keyCode === 32) {
+			if (event.keyCode === 32 && logOn == true) {
 				log.innerHTML = log.innerHTML + " ";
 				inverseLog.innerHTML = inverseLog.innerHTML + " ";
 			}
@@ -110,11 +120,14 @@
 				inverseDirection = "D";
 			}
 		}
-		var log = document.getElementById("log")
-		var inverseLog = document.getElementById("inverseLog")
-		log.innerHTML = log.innerHTML + direction;
-		inverseLog.innerHTML = inverseLog.innerHTML + inverseDirection;
 		logStack.push(direction);
+
+		if (logOn == true) {
+			var log = document.getElementById("log")
+			var inverseLog = document.getElementById("inverseLog")
+			log.innerHTML = log.innerHTML + direction;
+			inverseLog.innerHTML = inverseLog.innerHTML + inverseDirection;
+		}
 	}
 
 	function shuffleMovePuzzle(puzzle) {
@@ -134,6 +147,8 @@
 	}
 
 	function shuffle() {
+		logStack.splice(0,logStack.length);		// empty log stack 
+
 		for (var i = 150; i >= 0; i--) {
 			var neighbors = [];
 			var empty_neighbors = ["puzzle" + (empty_x - 1) + "_" + empty_y,
@@ -149,6 +164,22 @@
 			var puzzleToMove = neighbors[parseInt(Math.random() * neighbors.length)];
 			shuffleMovePuzzle(puzzleToMove);
 		}
+
+		/*var random_id = "";
+		var puzzle_id = ["0_0", "0_1", "0_2", "0_3",
+						 "1_0", "1_1", "1_2", "1_3", 
+						 "2_0", "2_1", "2_2", "2_3", 
+						 "3_0", "3_1", "3_2", "3_3"];
+		for (var i = 0; i <= 15; i++) {
+			random_id = puzzle_id[parseInt(Math.random() * puzzle_id.length)];
+			puzzle_id.splice(puzzle_id.indexOf(random_id), 1);
+			console.log(random_id);
+			console.log(puzzle_id);
+			document.getElementById("puzzle" + random_id).style.left = (i % 4) * 100 + "px";
+			document.getElementById("puzzle" + random_id).style.top = Math.floor(i / 4) * 100 + "px";
+			document.getElementById("puzzle" + random_id).id = "puzzle" + (i % 4).toString() + "_" + Math.floor(i / 4).toString();
+			console.log((i % 4), Math.floor(i / 4));
+		}*/
 	}
 
 	function calcPermuation() {
